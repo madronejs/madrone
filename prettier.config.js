5e7137caeac80fd368cf82226af968a076b6146b
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-23 16:52:23 -0700

    project configuration

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
