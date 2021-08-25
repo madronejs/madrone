0906ebb8ac858af5902883a1eaf08b6b6401923c
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-25 12:49:17 -0700

    add feats

--- src/Model.ts
+++ src/Model.ts
@@ -103,6 +103,7 @@ function createModel<ModelShape extends object>(shape: (ModelShape | MadroneType
       return Madrone.create({
         model,
         data,
+        feats: model.feats,
         type: model.mixed,
         install: (ctx) => installPlugins(ctx, featureCache, allPlugins()),
       });
