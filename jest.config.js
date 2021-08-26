df17f5a8940a31ab70f948e2d30c44c6028dd8ee
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-25 18:51:56 -0700

    more config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
   testMatch: [`<rootDir>/src/**/?(*.)spec.js`],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
