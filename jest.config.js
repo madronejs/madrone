d5f32e9b33873e7dbe1fe6f7bc2120379ad1fd6b
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-25 17:10:34 -0700

    ignore paths

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__spec__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
