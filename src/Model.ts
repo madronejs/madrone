49cd4b19f5192479af2dd30fe46291a318ff8dd7
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-26 23:17:54 -0700

    fix conflict

--- src/Model.ts
+++ src/Model.ts
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import { uniqueId } from 'lodash';
-import { makeMadrone, MadroneType } from './Madrone';
-import { getPlugins, getIntegrations } from './global';
-import { mixPlugins, installPlugins, analyzeObject } from './plugins';
-import { merge, flattenOptions } from './util';
-=======
 import { MadroneType, MadronePrototypeDescriptors } from './Madrone';
 import { getPlugins, getIntegrations } from './global';
 import { mixPlugins, installPlugins, analyzeObject } from './plugins';
 import { flattenOptions, getDefaultDescriptors, merge } from './util';
 
 let MODEL_COUNT = 0;
->>>>>>> main
 
 /**
  * @namespace
@@ -31,11 +23,7 @@ const Model = {
    */
   create: function create<ModelShape extends object>(shape: (ModelShape | MadroneType) | (() => ModelShape | MadroneType)) {
     /** Unique model identifier */
-<<<<<<< HEAD
-    const id = uniqueId('madrone_model');
-=======
     const id = `madrone_model_${++MODEL_COUNT}`;
->>>>>>> main
     /** Output of the mixed models */
     let shapeCache = {} as ModelShape;
     /** Output of mixed options */
@@ -68,7 +56,6 @@ const Model = {
         
         shapeCache = merge(...shapes) as ModelShape;
         return true;
-<<<<<<< HEAD
       }
       return false;
     };
@@ -85,50 +72,12 @@ const Model = {
       }
       return false;
     };
-=======
-      }
-      return false;
-    };
-    const compileOptions = () => {
-      if (optionQueue.length || !optionCache) {
-        while(optionQueue.length) {
-          let def = optionQueue.shift();
-          options.push(typeof def === 'function' ? def() : def);
-        }
-
-        // @ts-ignore
-        optionCache = getMergedOptions(...options, shapeCache.$options, analyzeObject(shapeCache));
-        return true;
-      }
-      return false;
-    };
->>>>>>> main
     const compile = () => {
       const dirtyShape = compileShape();
       const dirtyOptions = compileOptions();
       const dirty = dirtyShape || dirtyOptions;
       if (dirty) {
         typeCache = merge(shapeCache, { $options: optionCache });
-<<<<<<< HEAD
-      }
-      return dirty;
-    };
-    const getOptions = () => {
-      compile();
-      return optionCache;
-    };
-    const getShape = () => {
-      compile();
-      return shapeCache;
-    };
-    /** Extend a model definition by creating a new one */
-    const extend = <A extends object>(newShape: A | ModelShape | MadroneType) => {
-      if (Model.isModel(newShape)) {
-        return create(() => merge(getShape, () => (newShape as { type: A }).type) as A & ModelShape)
-          .withOptions(getOptions)
-          .withPlugins(plugins);
-=======
->>>>>>> main
       }
       return dirty;
     };
@@ -244,77 +193,6 @@ const Model = {
     Object.defineProperty(model, '$isModel', { value: true });
     mix(shape);
 
-<<<<<<< HEAD
-      return create(() => merge(getShape, newShape) as A & ModelShape)
-        .withOptions(getOptions)
-        .withPlugins(plugins);
-    };
-
-    const model = { 
-      /** Unique identifier for this model */
-      get id() {
-        return id as string;
-      },
-      extend,
-      /** The compiled output */
-      get shape() {
-        compile();
-        return shapeCache as ModelShape;
-      },
-      /** The compiled options */
-      get options() {
-        compile();
-        return optionCache;
-      },
-      /** The shape and options combined into a final "type" */
-      get type() {
-        compile();
-        return typeCache as ModelShape & { $options: any };
-      },
-      /** The plugins added to this model */
-      get plugins() {
-        return plugins;
-      },
-      /** Add plugins to this model */
-      withPlugins(...items) {
-        plugins.push(...items);
-        return model;
-      },
-      /** Add options to this model */
-      withOptions(...items) {
-        optionQueue.push(...items);
-        return model;
-      },
-      /** Create an instance of this model type */
-      create(data?: object, { app = null, root = null, parent = null } = {}) {
-        compileShape();
-        compileOptions();
-        return makeMadrone({
-          model,
-          data,
-          app,
-          root,
-          parent,
-          options: model.options,
-          type: model.type,
-          install: (ctx) => {
-            const [pl] = getIntegrations();
-
-            if (typeof pl?.integrate === 'function') {
-              ctx.$state = pl.integrate(ctx);
-            }
-
-            installPlugins(ctx, optionCache, allPlugins());
-          },
-        });
-      },
-    };
-
-    Object.defineProperty(model, '$isModel', { value: true });
-    mix(shape);
-
-=======
->>>>>>> main
     return model;
   }
 };
