13a886e3c57ae3f7171429052d600e074148b8dd
Tyler Reardon <tdr.reardon@gmail.com>
2021-08-27 15:53:36 -0700

    lint part 3

--- src/integrations/MadroneVue3.ts
+++ src/integrations/MadroneVue3.ts
@@ -1,59 +1,57 @@
 import lodashGet from 'lodash/get';
 
-export default ({ reactive, computed, watch } = {} as any) => {
-  return {
-    integrate: (ctx) => ({
-      ctx,
-      defineComputed(name, config) {
-        let getter;
-        let setter;
+export default ({ reactive, computed, watch } = {} as any) => ({
+  integrate: (ctx) => ({
+    ctx,
+    defineComputed(name, config) {
+      let getter;
+      let setter;
 
-        if (config.cache) {
-          const cp = computed(config);
+      if (config.cache) {
+        const cp = computed(config);
 
-          getter = () => cp.value;
-          setter = (val) => {
-            cp.value = val;
-          };
-        } else {
-          getter = config.get;
-          setter = config.set;
-        }
-
-        Object.defineProperty(this.ctx, name, {
-          enumerable: true,
-          configurable: true,
-          get: getter,
-          set: setter,
-        });
-      },
-
-      defineProperty(name, value) {
-        const target = { value };
-        const atom = reactive(target);
-
-        Object.defineProperty(this.ctx, name, {
-          enumerable: true,
-          configurable: true,
-          get: () => {
-            const { value: atomVal } = atom;
-
-            if (Array.isArray(atomVal)) {
-              // reactivity for arrays...
-              Reflect.get(atomVal, 'length');
-            }
-
-            return atomVal;
-          },
-          set: (val) => {
-            atom.value = val;
-          },
-        });
-      },
-
-      watch(path, { handler = undefined, deep = false } = {}) {
-        return watch(() => lodashGet(this.ctx, path), handler, { deep });
+        getter = () => cp.value;
+        setter = (val) => {
+          cp.value = val;
+        };
+      } else {
+        getter = config.get;
+        setter = config.set;
       }
-    }),
-  };
-}
+
+      Object.defineProperty(this.ctx, name, {
+        enumerable: true,
+        configurable: true,
+        get: getter,
+        set: setter,
+      });
+    },
+
+    defineProperty(name, value) {
+      const target = { value };
+      const atom = reactive(target);
+
+      Object.defineProperty(this.ctx, name, {
+        enumerable: true,
+        configurable: true,
+        get: () => {
+          const { value: atomVal } = atom;
+
+          if (Array.isArray(atomVal)) {
+            // reactivity for arrays...
+            Reflect.get(atomVal, 'length');
+          }
+
+          return atomVal;
+        },
+        set: (val) => {
+          atom.value = val;
+        },
+      });
+    },
+
+    watch(path, { handler = undefined, deep = false } = {}) {
+      return watch(() => lodashGet(this.ctx, path), handler, { deep });
+    },
+  }),
+});
