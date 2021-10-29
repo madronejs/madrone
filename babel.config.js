2647ef9d17bb2bab5cc602e67c78008485ac0f64
Tyler Reardon <tdr.reardon@gmail.com>
2021-10-29 16:47:09 -0700

    config changes for decorators

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,3 @@
-module.exports = { presets: ['@babel/preset-env'] };
+const decorators = require('@babel/plugin-proposal-decorators');
+
+module.exports = { presets: ['@babel/preset-env'], plugins: [[decorators, { legacy: true }]] };
