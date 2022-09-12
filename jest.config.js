ba199e226c42909ff5207877fd5883504c4d80d3
John Manuel <bitencode@johnmanuel.org>
2022-09-11 21:17:28 -0700

    build: babel is not needed any longer (#40)
    <!-- Remove all rows that don't apply to this PR --->
    | Type       | Description                                                                | Icon | Changelog |
    | ---------- | ---------------------------------------------------------------------------| :--: | :-------: |
    | `build`     | The build dependencies or scripts (package.json, etc...) are updated      |  🎁  |           |
    -------------------------------------------------------------------------------------------------------------
    
    ### 🛠️ Changes:
    <!-- Describe your changes in a brief, bullet list --->
    - remove babel and friends
    - remove babel dependencies from jest configuration
    
    ### 📢 Additional Info:
    <!-- Give some more context... Why is this needed? --->
    It appears to me that babel is not needed. Building without babel gives very similar results, but with different variable/function names in the resultant `dist` output.
    
    ### 📚 Bookkeeping:
    
    **Testing (if applicable)**:
    - [x] Ran/wrote unit tests for this
    
    **Checklist**
    - [x] Assigned PR to myself
    - [x] Added at least 1 person on the team as reviewer
    - [ ] Release Notes: PRs types that have the :spiral_notepad: next to them also require release notes to be added to the `CHANGELOG.md`

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
   testMatch: [`<rootDir>/src/**/?(*.)spec.(j|t)s`],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
