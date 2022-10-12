88c19d7b58edf69ccdad88d30a00f539577b6738
Tyler <tdr.reardon@gmail.com>
2022-10-12 15:45:53 -0700

    chore: configurable decorators (#42)
    <!-- Remove all rows that don't apply to this PR --->
    | Type | Description | Icon | Changelog |
    | ---------- |
    ---------------------------------------------------------------------------|
    :--: | :-------: |
    | `chore` | Internal stuff (feat/fix/debt/etc...) | 👷 | |
    
    -------------------------------------------------------------------------------------------------------------
    
    ### 🛠️ Changes:
    <!-- Describe your changes in a brief, bullet list --->
    - add the ability to configure the descriptors on properties and
    accessors defined using decorators
    - fix npm package preventing types from building successfully
    
    ### 📢 Additional Info:
    <!-- Give some more context... Why is this needed? --->
    This makes it possible to define a property and set its descriptors
    using decorators.
    
    ### 📚 Bookkeeping:
    
    **Testing (if applicable)**:
    - [x] Ran/wrote unit tests for this
    
    **Checklist**
    - [x] Assigned PR to myself
    - [x] Added at least 1 person on the team as reviewer
    - [ ] Release Notes: PRs types that have the :spiral_notepad: next to
    them also require release notes to be added to the `CHANGELOG.md`

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,6 +6,14 @@ export interface MadroneDescriptorMap {
   [key: string]: MadroneDescriptor;
 }
 
+export type DecoratorDescriptorType = Omit<
+  PropertyDescriptor,
+  'get' | 'set' | 'writable' | 'value'
+>;
+export type DecoratorOptionType = {
+  descriptors?: DecoratorDescriptorType;
+};
+
 export interface Integration {
   defineProperty: (
     target: any,
